a55426646cb72046db831cf5c566c0c2b21035f2
Frycen <ivanmatias1331@gmail.com>
2022-06-11 15:32:10 -0700

    finalized website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu} from './container';
+import {AboutUs, Chef, FindUs, Gallery, Header, Intro, Laurels, SpecialMenu} from './container';
 import {Navbar} from './components'
 
 import "./App.css"
@@ -17,7 +17,6 @@ function App() {
       <Laurels />
       <Gallery />
       <FindUs />
-      <Footer />
     </div>
   );
 }
